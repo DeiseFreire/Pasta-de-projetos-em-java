764e7944ef341af4b5decab48983e649d893ad33
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2021-05-01 21:18:29 -0300

    Create Curso de Java 05 - Primeiro Programa em Java.java

---
+++ Curso de Java 05 - Primeiro Programa em Java.java
@@ -0,0 +1,20 @@
+/*
+ * Fonte da ideia:
+ * Curso de Java 05: Primeiro Programa em   
+ * Java. Produção de Loiane Groner.  
+ * Publicado pelo canal Loiane Groner. [S. l: * s. n.], 2013. 1 * videoaula (27  
+ * mins. 41 segs.). Youtube.
+ */
+package aula05;
+
+/**
+ *
+ * @author deise
+ */
+class HelloWorld {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
