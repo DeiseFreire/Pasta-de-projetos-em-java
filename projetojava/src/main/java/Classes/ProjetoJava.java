af576e8374456124417c30704d9cf8663c4eedd2
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2021-05-03 21:29:22 -0300

    Finalizando formulario login e criando formulário menu java

---
+++ projetojava/src/main/java/Classes/ProjetoJava.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import formularios.frmLogin;
+
+/**
+ *
+ * @author deise
+ */
+public class ProjetoJava {
+
+    public static void main(String[] args) {
+
+        frmLogin Login = new frmLogin();
+        //Login.setlocationRelativeTo(null);
+        Login.setVisible(true);
+    }
+}
